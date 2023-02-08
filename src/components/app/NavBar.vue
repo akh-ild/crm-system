066f0c3694912ebb538e5a17aad7a965fa043f6f
akh-ild <ildar.akhmadeyev@gmail.com>
2023-02-08 18:22:04 +0600

    menu dropdown

--- src/components/app/NavBar.vue
+++ src/components/app/NavBar.vue
@@ -10,24 +10,20 @@
 
       <ul class="right hide-on-small-and-down">
         <li>
-          <a
-              class="dropdown-trigger black-text"
-              href="#"
-              data-target="dropdown"
-          >
+          <a ref="dropdown" class="dropdown-trigger black-text" href="#" data-target="dropdown">
             USER NAME
             <i class="material-icons right">arrow_drop_down</i>
           </a>
 
           <ul id='dropdown' class='dropdown-content'>
             <li>
-              <a href="#" class="black-text">
+              <router-link to="/profile" class="black-text">
                 <i class="material-icons">account_circle</i>Профиль
-              </a>
+              </router-link>
             </li>
             <li class="divider" tabindex="-1"></li>
             <li>
-              <a href="#" class="black-text">
+              <a href="#" class="black-text" @click.prevent="logout">
                 <i class="material-icons">assignment_return</i>Выйти
               </a>
             </li>
@@ -37,3 +33,19 @@
     </div>
   </nav>
 </template>
+
+<script>
+export default {
+  mounted () {
+    window.M.Dropdown.init(this.$refs.dropdown, {
+      constrainWidth: false
+    })
+  },
+  methods: {
+    logout () {
+      console.log('logout')
+      this.$router.push('/login?message=logout')
+    }
+  }
+}
+</script>
