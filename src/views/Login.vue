f091bed9e970015fe8a4917f4902627d7c74bbab
akh-ild <ildar.akhmadeyev@gmail.com>
2023-02-08 22:52:40 +0600

    login form vuelidate validation

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,16 +1,18 @@
 <template>
-  <form class="card auth-card">
+  <form class="card auth-card" @submit.prevent="onSubmit">
     <div class="card-content">
       <span class="card-title">Домашняя бухгалтерия</span>
       <div class="input-field">
-        <input id="email" type="text" class="validate">
+        <input id="email" type="text" v-model.trim="email" :class="{ invalid: v$.email.$error }" @focus="v$.email.$reset()">
         <label for="email">Email</label>
-        <small class="helper-text invalid">Email</small>
+        <small v-if="emptyEmail" class="helper-text invalid">Поле Email не должно быть пустым</small>
+        <small v-else-if="incorrectEmail" class="helper-text invalid">Введите корректный email</small>
       </div>
       <div class="input-field">
-        <input id="password" type="password" class="validate">
+        <input id="password" type="password" v-model="password" :class="{ invalid: v$.password.$error }" @focus="v$.password.$reset()">
         <label for="password">Пароль</label>
-        <small class="helper-text invalid">Password</small>
+        <small v-if="emptyPassword" class="helper-text invalid">Введите пароль</small>
+        <small v-else-if="shortPassword" class="helper-text invalid">Пароль должен содержать минимум {{ v$.password.minLength.$params.min }} символов</small>
       </div>
     </div>
     <div class="card-action">
@@ -23,8 +25,60 @@
 
       <p class="center">
         Нет аккаунта?
-        <a href="/">Зарегистрироваться</a>
+        <router-link to="/register">Зарегистрироваться</router-link>
       </p>
     </div>
   </form>
 </template>
+
+<script>
+import { useVuelidate } from '@vuelidate/core'
+import { required, email, minLength } from '@vuelidate/validators'
+
+export default {
+  name: 'login',
+  data () {
+    return {
+      email: '',
+      password: ''
+    }
+  },
+  setup () {
+    return { v$: useVuelidate() }
+  },
+  computed: {
+    emptyEmail () {
+      return this.v$.email.$dirty && !this.v$.email.required.$response
+    },
+    incorrectEmail () {
+      return this.v$.email.$dirty && this.v$.email.email.$invalid
+    },
+    emptyPassword () {
+      return this.v$.password.$dirty && !this.v$.password.required.$response
+    },
+    shortPassword () {
+      return this.v$.password.$dirty && this.v$.password.minLength.$invalid
+    }
+  },
+  methods: {
+    onSubmit () {
+      if (this.v$.$invalid) {
+        this.v$.$touch()
+        return
+      }
+      const formData = {
+        email: this.email,
+        password: this.password
+      }
+      console.log(formData)
+      this.$router.push('/')
+    }
+  },
+  validations () {
+    return {
+      email: { required, email },
+      password: { required, minLength: minLength(6) }
+    }
+  }
+}
+</script>
