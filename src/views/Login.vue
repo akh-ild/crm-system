eb91dede25d1f8493857e2e03533cac39b5668e1
akh-ild <ildar.akhmadeyev@gmail.com>
2023-02-09 15:52:37 +0600

    auth

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -67,7 +67,7 @@ export default {
     }
   },
   methods: {
-    onSubmit () {
+    async onSubmit () {
       if (this.v$.$invalid) {
         this.v$.$touch()
         return
@@ -76,8 +76,12 @@ export default {
         email: this.email,
         password: this.password
       }
-      console.log(formData)
-      this.$router.push('/')
+      try {
+        await this.$store.dispatch('login', formData)
+        this.$router.push('/')
+      } catch (e) {
+        console.log('error', e)
+      }
     }
   },
   validations () {
